c121c04445ca2127157934a43a2c9450005becc3
CNEFX <jellisvandenberg@gmail.com>
2026-06-17 19:28:35 +0200

    Add game assets with HTML links and placeholders

---
+++ game-assets.js
@@ -0,0 +1,9 @@
+const gameAssets = {
+  "tetris.html": "https://placehold.co/300x180/1b1f3a/00ff88?text=TETRIS",
+  "snake.html": "https://placehold.co/300x180/0a0a0f/00aaff?text=SNAKE",
+  "breakout.html": "https://placehold.co/300x180/111/ff4444?text=BREAKOUT",
+  "hexgl.html": "https://placehold.co/300x180/0f0f2a/ffcc00?text=HEXGL",
+  "stickman-world.html": "https://placehold.co/300x180/1a1a1a/ff00aa?text=STICKMAN",
+  "paperio2.html": "https://placehold.co/300x180/000/ffffff?text=PAPER.IO+2",
+  "moto-x3m.html": "https://placehold.co/300x180/0d0d0d/00ff88?text=MOTO+X3M"
+};
